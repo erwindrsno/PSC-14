3fae62cd2e77f91550c45277dee7983177d3eefa
Angelina Jeany <106376798+angelina734@users.noreply.github.com>
2022-12-17 09:20:31 +0700

    DONE BERES GAIS

--- Project/src/App.java
+++ Project/src/App.java
@@ -5,21 +5,21 @@ public class App{
         Scanner sc = new Scanner(System.in);
         
         //input panjang array 2 dimensi untuk generate minesweeper
-        int sizeArray = 5;
+        int sizeArray = sc.nextInt();
         //generate array berdasarkan size
 
         // i = baris
         // j = kolom
         
         //instansiasi array 2 dimensi
-        int[][] minesweeperBoard = new int[5+2][5+2];
-        String[][] minesweeperCheck = new String[5+2][5+2];
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 7; j++) {
-                if(i==0||i==6||j==0||j==6){
+        int[][] minesweeperBoard = new int[sizeArray+2][sizeArray+2];
+        String[][] minesweeperCheck = new String[sizeArray+2][sizeArray+2];
+        for (int i = 0; i < minesweeperBoard.length; i++) {
+            for (int j = 0; j < minesweeperBoard.length; j++) {
+                if(i==0||i==minesweeperBoard.length-1||j==0||j==minesweeperBoard.length-1){
                     minesweeperBoard[i][j] = -1;
                     minesweeperCheck[i][j]="bisajadi";
-                }else if(i!=0||i!=6||j!=0||j!=6){
+                }else if(i!=0||i!=minesweeperBoard.length-1||j!=0||j!=minesweeperBoard.length-1){
                     //input angka yang ingin di input dimana (i,j) i = baris, j = kolom
                     int num = sc.nextInt();
                     
@@ -33,10 +33,10 @@ public class App{
         printBoard(minesweeperBoard);
         do{
             solve(minesweeperBoard,minesweeperCheck);
-            change(minesweeperBoard,minesweeperCheck);
-            printBoard2(minesweeperCheck);            
+//            printBoard2(minesweeperCheck);            
         } while(checkAll(minesweeperCheck) == false);
-        
+        change(minesweeperBoard,minesweeperCheck);
+        printBoard(minesweeperBoard);
 //        while(checkAll(minesweeperCheck)==false){
 //            solve(minesweeperBoard,minesweeperCheck);
 //        }
@@ -57,8 +57,8 @@ public class App{
         return check;
     }
     public static void printBoard(int[][] board){
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
+        for (int i = 1; i < board.length-1; i++) {
+            for (int j = 1; j < board.length-1; j++) {
                 System.out.print(board[i][j]+" ");
             }
             System.out.println();
@@ -73,8 +73,8 @@ public class App{
         }
     }
     public static void solve(int[][] minesweeperBoard, String[][] minesweeperCheck){
-        for (int i = 1; i < 7-1; i++) {
-            for (int j = 1; j < 7-1; j++) {
+        for (int i = 1; i < minesweeperBoard.length-1; i++) {
+            for (int j = 1; j < minesweeperBoard.length-1; j++) {
                 //kalo jumlah kotak di sekitar angkaX == angkaX, kotak diisi string "ya"
                 //kalo angkaX == 0, kotak diisi string "tidak"
                 if(minesweeperBoard[i][j]==0){
@@ -105,7 +105,7 @@ public class App{
                     if(minesweeperCheck[i-1][j-1].equals("bisajadi")){
                         minesweeperCheck[i-1][j-1]="tidak";
                     }
-                }else if(minesweeperBoard[i][j]==4 && ((i==1 && (j==1||j==5))||(i==5 && (j==1||j==5)))){
+                }else if(minesweeperBoard[i][j]==4 && ((i==1 && (j==1||j==minesweeperBoard.length-2))||(i==minesweeperBoard.length-2 && (j==1||j==minesweeperBoard.length-2)))){
                     minesweeperCheck[i][j]="iya";
                     minesweeperCheck[i+1][j]="iya";
                     minesweeperCheck[i-1][j]="iya";
@@ -116,7 +116,7 @@ public class App{
                     minesweeperCheck[i-1][j+1]="iya";
                     minesweeperCheck[i-1][j-1]="iya";
                 }
-                else if(minesweeperBoard[i][j]==6 && (i==1&&j>1&&j<5||i==5&&j>1&&j<5||j==1&&i>1&&i<5||j==5&&i>1&&i<5)){
+                else if(minesweeperBoard[i][j]==6 && (i==1&&j>1&&j<minesweeperBoard.length-2||i==minesweeperBoard.length-2&&j>1&&j<minesweeperBoard.length-2||j==1&&i>1&&i<minesweeperBoard.length-2||j==minesweeperBoard.length-2&&i>1&&i<minesweeperBoard.length-2)){
                     minesweeperCheck[i][j]="iya";
                     minesweeperCheck[i+1][j]="iya";
                     minesweeperCheck[i-1][j]="iya";
@@ -132,9 +132,9 @@ public class App{
                     int hitungIya = 0;
                     int hitungTidak = 0;
                     int hitungKotak = 0;
-                    if(i>1&&i<5&&j>1&&j<5){
+                    if(i>1&&i<minesweeperBoard.length-2&&j>1&&j<minesweeperBoard.length-2){
                         hitungKotak=9;
-                    }else if((i==1&&(j==1||j==5))||(i==5 && (j==1||j==5))){
+                    }else if((i==1&&(j==1||j==minesweeperBoard.length-2))||(i==minesweeperBoard.length-2 && (j==1||j==minesweeperBoard.length-2))){
                         hitungKotak=4;
                     }else{
                         hitungKotak=6;
@@ -144,9 +144,9 @@ public class App{
                     }else if(minesweeperCheck[i][j].equals("tidak")){
                         hitungTidak++;
                     }
-                    if(minesweeperCheck[i+1][j].equals("iya")&&i+1!=6){
+                    if(minesweeperCheck[i+1][j].equals("iya")&&i+1!=minesweeperBoard.length-1){
                         hitungIya++;
-                    }else if(minesweeperCheck[i+1][j].equals("tidak")&&i+1!=6){
+                    }else if(minesweeperCheck[i+1][j].equals("tidak")&&i+1!=minesweeperBoard.length-1){
                         hitungTidak++;
                     }
                     if(minesweeperCheck[i-1][j].equals("iya")&&i-1!=0){
@@ -154,9 +154,9 @@ public class App{
                     }else if(minesweeperCheck[i-1][j].equals("tidak")&&i-1!=0){
                         hitungTidak++;
                     }
-                    if(minesweeperCheck[i][j+1].equals("iya")&&j+1!=6){
+                    if(minesweeperCheck[i][j+1].equals("iya")&&j+1!=minesweeperBoard.length-1){
                         hitungIya++;
-                    }else if(minesweeperCheck[i][j+1].equals("tidak")&&j+1!=6){
+                    }else if(minesweeperCheck[i][j+1].equals("tidak")&&j+1!=minesweeperBoard.length-1){
                         hitungTidak++;
                     }
                     if(minesweeperCheck[i][j-1].equals("iya")&&j-1!=0){
@@ -164,19 +164,19 @@ public class App{
                     }else if(minesweeperCheck[i][j-1].equals("tidak")&&j-1!=0){
                         hitungTidak++;
                     }
-                    if(minesweeperCheck[i+1][j+1].equals("iya")&&i+1!=6&&j+1!=6){
+                    if(minesweeperCheck[i+1][j+1].equals("iya")&&i+1!=minesweeperBoard.length-1&&j+1!=minesweeperBoard.length-1){
                         hitungIya++;
-                    }else if(minesweeperCheck[i+1][j+1].equals("tidak")&&i+1!=6&&j+1!=6){
+                    }else if(minesweeperCheck[i+1][j+1].equals("tidak")&&i+1!=minesweeperBoard.length-1&&j+1!=minesweeperBoard.length-1){
                         hitungTidak++;
                     }
-                    if(minesweeperCheck[i+1][j-1].equals("iya")&&i+1!=6&&j-1!=0){
+                    if(minesweeperCheck[i+1][j-1].equals("iya")&&i+1!=minesweeperBoard.length-1&&j-1!=0){
                         hitungIya++;
-                    }else if(minesweeperCheck[i+1][j-1].equals("tidak")&&i+1!=6&&j-1!=0){
+                    }else if(minesweeperCheck[i+1][j-1].equals("tidak")&&i+1!=minesweeperBoard.length-1&&j-1!=0){
                         hitungTidak++;
                     }
-                    if(minesweeperCheck[i-1][j+1].equals("iya")&&i-1!=0&&j+1!=6){
+                    if(minesweeperCheck[i-1][j+1].equals("iya")&&i-1!=0&&j+1!=minesweeperBoard.length-1){
                         hitungIya++;
-                    }else if(minesweeperCheck[i-1][j+1].equals("tidak")&&i-1!=0&&j+1!=6){
+                    }else if(minesweeperCheck[i-1][j+1].equals("tidak")&&i-1!=0&&j+1!=minesweeperBoard.length-1){
                         hitungTidak++;
                     }
                     if(minesweeperCheck[i-1][j-1].equals("iya")&&i-1!=0&&j-1!=0){
@@ -189,25 +189,25 @@ public class App{
                         if(minesweeperCheck[i][j].equals("bisajadi")){
                             minesweeperCheck[i][j]="tidak";
                         }
-                        if(minesweeperCheck[i+1][j].equals("bisajadi")&&i+1!=6){
+                        if(minesweeperCheck[i+1][j].equals("bisajadi")&&i+1!=minesweeperBoard.length-1){
                             minesweeperCheck[i+1][j]="tidak";
                         }
                         if(minesweeperCheck[i-1][j].equals("bisajadi")&&i-1!=0){
                             minesweeperCheck[i-1][j]="tidak";
                         }
-                        if(minesweeperCheck[i][j+1].equals("bisajadi")&&j+1!=6){
+                        if(minesweeperCheck[i][j+1].equals("bisajadi")&&j+1!=minesweeperBoard.length-1){
                             minesweeperCheck[i][j+1]="tidak";
                         }
                         if(minesweeperCheck[i][j-1].equals("bisajadi")&&j-1!=0){
                             minesweeperCheck[i][j-1]="tidak";
                         }
-                        if(minesweeperCheck[i+1][j+1].equals("bisajadi")&&i+1!=6&&j+1!=6){
+                        if(minesweeperCheck[i+1][j+1].equals("bisajadi")&&i+1!=minesweeperBoard.length-1&&j+1!=minesweeperBoard.length-1){
                             minesweeperCheck[i+1][j+1]="tidak";
                         }
-                        if(minesweeperCheck[i+1][j-1].equals("bisajadi")&&i+1!=6&&j-1!=0){
+                        if(minesweeperCheck[i+1][j-1].equals("bisajadi")&&i+1!=minesweeperBoard.length-1&&j-1!=0){
                             minesweeperCheck[i+1][j-1]="tidak";
                         }
-                        if(minesweeperCheck[i-1][j+1].equals("bisajadi")&&i-1!=0&&j+1!=6){
+                        if(minesweeperCheck[i-1][j+1].equals("bisajadi")&&i-1!=0&&j+1!=minesweeperBoard.length-1){
                             minesweeperCheck[i-1][j+1]="tidak";
                         }
                         if(minesweeperCheck[i-1][j-1].equals("bisajadi")&&i-1!=0&&j-1!=0){
@@ -219,25 +219,25 @@ public class App{
                         if(minesweeperCheck[i][j].equals("bisajadi")){
                             minesweeperCheck[i][j]="iya";
                         }
-                        if(minesweeperCheck[i+1][j].equals("bisajadi")&&i+1!=6){
+                        if(minesweeperCheck[i+1][j].equals("bisajadi")&&i+1!=minesweeperBoard.length-1){
                             minesweeperCheck[i+1][j]="iya";
                         }
                         if(minesweeperCheck[i-1][j].equals("bisajadi")&&i-1!=0){
                             minesweeperCheck[i-1][j]="iya";
                         }
-                        if(minesweeperCheck[i][j+1].equals("bisajadi")&&j+1!=6){
+                        if(minesweeperCheck[i][j+1].equals("bisajadi")&&j+1!=minesweeperBoard.length-1){
                             minesweeperCheck[i][j+1]="iya";
                         }
                         if(minesweeperCheck[i][j-1].equals("bisajadi")&&j-1!=0){
                             minesweeperCheck[i][j-1]="iya";
                         }
-                        if(minesweeperCheck[i+1][j+1].equals("bisajadi")&&i+1!=6&&j+1!=6){
+                        if(minesweeperCheck[i+1][j+1].equals("bisajadi")&&i+1!=minesweeperBoard.length-1&&j+1!=minesweeperBoard.length-1){
                             minesweeperCheck[i+1][j+1]="iya";
                         }
-                        if(minesweeperCheck[i+1][j-1].equals("bisajadi")&&i+1!=6&&j-1!=0){
+                        if(minesweeperCheck[i+1][j-1].equals("bisajadi")&&i+1!=minesweeperBoard.length-1&&j-1!=0){
                             minesweeperCheck[i+1][j-1]="iya";
                         }
-                        if(minesweeperCheck[i-1][j+1].equals("bisajadi")&&i-1!=0&&j+1!=6){
+                        if(minesweeperCheck[i-1][j+1].equals("bisajadi")&&i-1!=0&&j+1!=minesweeperBoard.length-1){
                             minesweeperCheck[i-1][j+1]="iya";
                         }
                         if(minesweeperCheck[i-1][j-1].equals("bisajadi")&&i-1!=0&&j-1!=0){
