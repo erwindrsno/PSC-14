a17a52ab593a1b6c86def56d99c01dd668ef2208
Erwin Darsono <6182001002@student.unpar.ac.id>
2022-12-19 03:20:39 +0700

    ubah input

--- Project/src/App.java
+++ Project/src/App.java
@@ -4,7 +4,42 @@ import java.util.Scanner;
 
 public class App{
     public static void main(String[] args) throws FileNotFoundException{
-        File file = new File("D:/Erwin Darsono/minesweeper.txt");
+        // Scanner sc = new Scanner(System.in);
+        
+        // //input panjang array 2 dimensi untuk generate minesweeper
+        // System.out.println("Input size array");
+        // int boardSize = sc.nextInt();
+        // System.out.println("Input population size");
+        // int populationSize = sc.nextInt();
+        // //generate array berdasarkan size
+
+        // // i = baris
+        // // j = kolom
+        
+        // //instansiasi papan permainan, papan untuk memeriksa jawabannya, dan papan yang menampung solusi dalam bentuk binary
+        // int[][] minesweeperBoard = new int[boardSize+2][boardSize+2];
+        // String[][] minesweeperCheck = new String[boardSize+2][boardSize+2];
+        // int[][] answer = new int[boardSize+2][boardSize+2];
+
+        // System.out.println("Input board");
+        // for (int i = 0; i < minesweeperBoard.length; i++) {
+        //     for (int j = 0; j < minesweeperBoard.length; j++) {
+        //         if(i==0||i==minesweeperBoard.length-1||j==0||j==minesweeperBoard.length-1){
+        //             minesweeperBoard[i][j] = -1;
+        //             minesweeperCheck[i][j]="bisajadi";
+        //         }else if(i!=0||i!=minesweeperBoard.length-1||j!=0||j!=minesweeperBoard.length-1){
+        //             //input angka yang ingin di input dimana (i,j) i = baris, j = kolom
+        //             int num = sc.nextInt();
+        //             //put num to board
+        //             minesweeperBoard[i][j] = num;
+
+        //             minesweeperCheck[i][j]="bisajadi";
+        //         }
+        //     }
+        // }
+
+
+        File file = new File("D:/Users/Erwin Darsono/Desktop/PSC-14");
         Scanner sc = new Scanner(file);
         String input = sc.nextLine();
         String[] arrSplit = input.split(",");
@@ -26,7 +61,7 @@ public class App{
                 }
                 else if(i!=0||i!=minesweeperBoard.length-1||j!=0||j!=minesweeperBoard.length-1){
                     int num = Integer.parseInt(arrSplit[index]);
-                    // System.out.println(num);
+                    System.out.println(num);
                     index++;
                     //put num to board
                     minesweeperBoard[i][j] = num;
@@ -34,15 +69,15 @@ public class App{
             }
         }
 
-        // printBoard(minesweeperBoard);
+        printBoard(minesweeperBoard);
 
-        // Inisialisasi objek Solver untuk menyelesaikan puzzle tanpa menggunakan algoritma genetik
-        Solver solver = new Solver(minesweeperBoard, minesweeperCheck, answer, (int)Math.pow(boardSize,2));
-        solver.begin();
+        //Inisialisasi objek Solver untuk menyelesaikan puzzle tanpa menggunakan algoritma genetik
+        // Solver solver = new Solver(minesweeperBoard, minesweeperCheck, answer, (int)Math.pow(boardSize,2));
+        // solver.begin();
 
-        int[] target = solver.convertToArray();
+        // int[] target = solver.convertToArray();
 
-        initializePopulation(populationSize, boardSize, target);
+        // initializePopulation(populationSize, boardSize, target);
     }
 
     public static void initializePopulation(int populationSize, int boardSize, int[] target){
@@ -52,13 +87,13 @@ public class App{
         population.printInfo();
     }
 
-    // public static void printBoard(int[][] minesweeperBoard){
-    //     System.out.println("Tes papan");
-    //     for (int i = 1; i < minesweeperBoard.length-1; i++) {
-    //         for (int j = 1; j < minesweeperBoard.length-1; j++) {
-    //             System.out.print(minesweeperBoard[i][j]+"\t");
-    //         }
-    //         System.out.println();
-    //     }
-    // }
+    public static void printBoard(int[][] minesweeperBoard){
+        System.out.println("Tes papan");
+        for (int i = 0; i < minesweeperBoard.length; i++) {
+            for (int j = 0; j < minesweeperBoard.length; j++) {
+                System.out.print(minesweeperBoard[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
